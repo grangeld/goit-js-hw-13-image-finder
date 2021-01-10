76d67fc4afd558da359b634c5b4434b5084b0f6f
Dima <grangeld3@gmail.com>
2021-01-10 21:31:39 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// const postToAdd = {
+//   author: 'Mango',
+//   content: 'CRUD is awesome',
+// };
+
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//   method: 'POST',
+//   body: JSON.stringify(postToAdd),
+//   headers: {
+//     'Content-Type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then(response => response.json())
+//   .then(post => console.log(post))
+//   .catch(error => console.log(error));
